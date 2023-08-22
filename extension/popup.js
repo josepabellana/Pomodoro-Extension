dc4d1f991a7f8de9b54409948879d4c6fa04e419
josepabellana <josep.abe.pu@gmail.com>
2023-08-22 21:48:44 +0200

    timer done

---
+++ extension/popup.js
@@ -0,0 +1,80 @@
+// variables
+
+const startButton = document.getElementById('start');
+const resetButton = document.getElementById('start');
+const workTittle = document.getElementById('work');
+const breakTittle = document.getElementById('break');
+
+const workTime = 25;
+const breakTime = 5;
+
+const seconds = "00"
+
+// display
+window.onload = () => {
+    document.getElementById('minutes').innerHTML = workTime;
+    document.getElementById('seconds').innerHTML = seconds;
+
+    workTittle.classList.add('active');
+}
+
+startButton.addEventListener('click', () => start());
+// start timer
+function start() {
+    // change button
+    document.getElementById('start').style.display = "none";
+    document.getElementById('reset').style.display = "block";
+
+    // change the time
+    seconds = 59;
+
+    let workMinutes = workTime - 1;
+    let breakMinutes = breakTime - 1;
+
+    breakCount = 0;
+
+    // countdown
+    let timerFunction = () => {
+        //change the display
+        document.getElementById('minutes').innerHTML = workMinutes;
+        document.getElementById('seconds').innerHTML = seconds;
+
+        // start
+        seconds = seconds - 1;
+
+        if(seconds === 0) {
+            workMinutes = workMinutes - 1;
+            if(workMinutes === -1 ){
+                if(breakCount % 2 === 0) {
+                    // start break
+                    workMinutes = breakMinutes;
+                    breakCount++
+
+                    // change the painel
+                    workTittle.classList.remove('active');
+                    breakTittle.classList.add('active');
+                }else {
+                    // continue work
+                    workMinutes = workTime;
+                    breakCount++
+
+                    // change the painel
+                    breakTittle.classList.remove('active');
+                    workTittle.classList.add('active');
+                }
+            }
+            seconds = 59;
+        }
+    }
+
+    // start countdown
+    setInterval(timerFunction, 1000); // 1000 = 1s
+}
+
+
+
+resetButton.addEventListener('click', () => reset());
+// reset timer
+function reset(){
+
+}
